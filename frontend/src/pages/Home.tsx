772d305019b59b8a2fee82b78c964ad1fcb0e967
Brijkishor636 <kbrijkishor636@gmail.com>
2025-11-02 12:25:06 +0530

    completing home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,17 @@
 import Banner from "../components/Home/Banner";
+import CallToAction from "../components/Home/CallToAction";
+import Features from "../components/Home/Features";
+import Footer from "../components/Home/Footer";
 import Hero from "../components/Home/Hero";
+import Testimonials from "../components/Home/Testimonials";
 
 export default function Home(){
     return <div>
         <Banner/>
         <Hero/>
+        <Features/>
+        <Testimonials/>
+        <CallToAction/>
+        <Footer/>
     </div>
 }
